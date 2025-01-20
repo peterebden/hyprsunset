cac797e03472398854cae466475ade91bf2a36be
Peter Ebden <peter.ebden@gmail.com>
2025-01-20 07:54:50 +0000

    yes of course it doesn't support indentation

--- src/main.cpp
+++ src/main.cpp
@@ -185,12 +185,7 @@ void writeFile(const string& file, int kelvin, int minTemp, int maxTemp) {
     try {
       const double percentage = 100.0 * (double)(kelvin - minTemp) / (double)(maxTemp - minTemp);
       ofstream f(file);
-      f << std::format(R"({{
-  "text": "{}K",
-  "tooltip": "Current temperature: {}K",
-  "class: "p{}",
-  "percentage": {},
-}})", kelvin, kelvin, percentage, percentage);
+      f << std::format(R"({{"text": "{}K","tooltip": "Current temperature: {}K","class: "p{}","percentage": {}}})", kelvin, kelvin, percentage, percentage);
       f.close();
     } catch (std::exception& ex) {
       Debug::log(WARN, "✖ Couldn't write output file: {}", ex.what());
